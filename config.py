4f881de4e5878f52e158285a390fb596e6582d10
human-d3v <brytop05@gmail.com>
2025-07-28 14:03:28 -0600

    Added output, pywm, general appearance, and view settings

--- config.py
+++ config.py
@@ -76,3 +76,58 @@ def on_reconfigure():
     for s in services:
         service = f"{s} &"
         os.system(service)
+
+
+# ======== screen dimensions ==============================================
+outputs = [
+    {'name': 'eDP-1', 'scale': 1.0, 'width': 1920, 'height': 1080, 'mHz': 60,
+     'pos_x': 0, 'pos_y': 0, 'anim': True},
+]
+
+
+# ============= pywm settings =============================================
+# https://github.com/newm-next/newm-next/blob/master/doc/config.md
+pywm = {
+        'enable_xwayland': True,
+        'xkb_model': "",
+        'xkb_layout': "",
+        'xkb_options': "caps:ctrl_modifier, ctrl:swap_lwin_lctl",  # custom
+        'xcursor_theme': cursor_theme,
+        'xcursor_size': 14,
+        'tap_to_click': True,
+        'natural_scroll': True,
+        'focus_follows_mouse': True,
+        'constrain_popups_to_toplevel': True,
+        'encourage_csd': False,  # disable client side decorations
+        'debug': False,
+        'shaders': 'basic',
+        'renderer_mode': 'pywm',  # enable pywm (alternative is wlr)
+}
+
+# ============== general appearance settings ==============================
+# https://github.com/newm-next/newm-next/blob/master/doc/config.md
+# -- set background
+background = {
+    'path': os.environ["HOME"] +
+    "/.config/newm_next_dots/wallpaper/red_rocks.jpg",
+    'time_scale': 0.15,  # time scale of background movement
+    'anim': True,
+}
+# -- other settings
+blend_time = 1.0  # time in seconds to blend in and out at startup & shutdown
+anim_time = 0.10  # time in seconds for all animations (default is .3 == too slow)
+corner_radius = 8
+
+# ============= view settings ============================================
+# first, define rules:
+# https://github.com/newm-next/newm-next/blob/master/doc/tips_and_tricks.md
+def rules(view):
+    common_rules = {
+        "float": True,
+        "float_size": (750, 750),
+        "float_pos": (0.5, 0.35)
+    }
+    float_apps = ("rofi-Networks", "Rofi", "nm-connection-editor")
+
+    if view.app_id in float_apps:
+        return common_rules
