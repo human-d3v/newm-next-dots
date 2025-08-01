a48dcf2805fbfe9645a6a3ad55bb33136ce5c090
human-d3v <brytop05@gmail.com>
2025-08-01 15:16:05 -0600

    Added waybar configuration

---
+++ scripts/waybar.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+_ps=(waybar)
+for _prs in "${_ps[@]}"; do 
+	if [[ `pidof ${_prs}` ]]; then 
+		killall -9 ${_prs}
+	fi
+done
+
+waybar -c ~/.config/newm-next-dots/waybar/config &
