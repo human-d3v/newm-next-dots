6a0dbfb2e53584e2bf8ad98f91bd658bc977057e
human-d3v <brytop05@gmail.com>
2026-03-06 22:49:26 -0700

    Added additional scripts to launcher

--- launcher.py
+++ launcher.py
@@ -5,7 +5,11 @@ entries = {
     "Gimp": "gimp",
     "Thunar": "thunar",
     "Alacritty": "alacritty",
-    "Tor": f"{os.environ["HOME"]}/.config/newm/scripts/tor.sh"
+    "Tor": f"{os.environ["HOME"]}/.config/newm/scripts/tor.sh",
+    "Foot": "foot",
+    "Zoom": f"{os.environ["HOME"]}/.config/newm/scripts/zoom.sh",
+    "Calibre": f"{os.environ["HOME"]}/.config/newm/scripts/calibre.sh",
+    "VLC": f"{os.environ["HOME"]}/.config/newm/scripts/vlc.sh"
 }
 
 shortcuts = {
